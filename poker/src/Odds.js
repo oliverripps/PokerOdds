ecb48305702e4842e535c3cca9fcb89fb28154ca
Oliver Ripps <oliver.ripps@com>
2021-04-14 15:45:00 -0400

    massive functionality progress. needs UI work

--- poker/src/Odds.js
+++ poker/src/Odds.js
@@ -7,37 +7,100 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 
 function Odds() {
-  const seenCards = ['5h', '4h'];
-  let allCards = new Set();
-  const cardvals = [
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-    'T',
-    'J',
-    'Q',
-    'K',
-    'A',
-  ];
-  const suitevals = ['d', 'c', 'h', 's'];
-  cardvals.forEach((cardval) => {
-    suitevals.forEach((suiteval) => {
-      let currCard = cardval + suiteval;
-      if (seenCards.indexOf(currCard) === -1) {
-        allCards.add(currCard);
-      }
+  let userReturn = '';
+  const getAllCards = (hand, board) => {
+    let seenCards = hand.concat(board);
+    let allCards = new Set();
+    const cardvals = [
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'T',
+      'J',
+      'Q',
+      'K',
+      'A',
+    ];
+    const suitevals = ['d', 'c', 'h', 's'];
+    cardvals.forEach((cardval) => {
+      suitevals.forEach((suiteval) => {
+        let currCard = cardval + suiteval;
+        if (seenCards.indexOf(currCard) === -1) {
+          allCards.add(currCard);
+        }
+      });
     });
-  });
-  let allCardArray = Array.from(allCards);
-  let allHands = new Set();
+    return Array.from(allCards);
+  };
 
-  //We need to take in the number of players that are playing in the hand. Create a list of all of the combinations of hands x amount of other players could have.
-  // We then count how many of the hands we had the highest odds, how many we didn't and how many we tied. Thats the %s.
+  const findProbability = (p, s, hand, allCardArray) => {
+    const myhand = hand;
+    let players = p;
+    let handList = [];
+    let sum = 0;
+    let factor = s;
+    for (let i = 1; i <= factor; i++) {
+      handList = [];
+      let freshcards = [...allCardArray];
+      for (let l = 0; l < players - 1; l++) {
+        let select = Math.floor(Math.random() * freshcards.length);
+        let first = freshcards[select];
+        freshcards.splice(select, 1);
+        select = Math.floor(Math.random() * freshcards.length);
+        let second = freshcards[select];
+        freshcards.splice(select, 1);
+        let hand = first + second;
+        handList.unshift(CardGroup.fromString(hand));
+      }
+      handList.unshift(CardGroup.fromString(myhand));
+      sum += OddsCalculator.calculate(handList).equities[0].getEquity();
+      if (i === 1) {
+        console.log('Simulated 1 time');
+      } else {
+        console.log('Simulated ' + i + ' times');
+      }
+    }
+    const COW = sum / factor;
+    if (s === 1) {
+      return (
+        'After ' +
+        factor +
+        ' simulation, your average probability to win is ' +
+        COW
+      );
+    } else {
+      return (
+        'After ' +
+        factor +
+        ' simulations, your average probability to win is ' +
+        COW
+      );
+    }
+  };
+
+  const calculateOdds = (players, numOfSims, hand, board) => {
+    let totalplayers = players;
+    let sims = numOfSims;
+    const myhand = [hand.substring(0, 2), hand.substring(2)];
+    let all = getAllCards(myhand, board);
+    const before = Date.now();
+    let ret = findProbability(totalplayers, sims, hand, all);
+    const after = Date.now();
+    console.log(
+      'The ' + sims + ' simulations took',
+      (after - before) / 1000,
+      'seconds'
+    );
+    return ret;
+  };
+
+  /* let allHands = new Set();
+
   allCardArray.forEach((card1) => {
     allCardArray.forEach((card2) => {
       let reverse = card2 + card1;
@@ -51,15 +114,19 @@ function Odds() {
       }
     });
   });
-  let allHandArray = Array.from(allHands);
-  let allHandInput = new Set();
+  let allHandArray = Array.from(allHands); */
+  //We need to take in the number of players that are playing in the hand. Create a list of all of the combinations of hands x amount of other players could have.
+  // We then count how many of the hands we had the highest odds, how many we didn't and how many we tied. Thats the %s.
+  /* let allHandInput = new Set();
   allHandArray.forEach((hand) => {
     allHandInput.add(CardGroup.fromString(hand));
   });
   let allHandInputArray = Array.from(allHandInput);
-  console.log(allHandInput);
-  const result = OddsCalculator.calculate(allHandInputArray);
-  const [playerCards, setCards] = useState({ player: '', cards: [], odds: '' });
+  console.log(allHandInput); */
+  //const result = OddsCalculator.calculate(allHandInputArray);
+  //const [playerCards, setCards] = useState({ player: '', cards: [], odds: '' });
+  const [players, setPlayers] = useState('');
+  const [oddsResults, setResults] = useState('');
   const [name, setName] = useState('');
   const [card, setCard] = useState({
     value: '',
@@ -76,8 +143,11 @@ function Odds() {
     [playerCards.cards[0], playerCards.cards[1]],
     bboard
   ); */
+  const handlePlayer = (event) => {
+    let players = event.target.value;
+    setPlayers(players);
+  };
   const handleChangeValue = (event) => {
-    console.log(event);
     let val = event.target.value;
     let suite = card.suite;
     setCard({ value: val, suite: suite });
@@ -88,23 +158,48 @@ function Odds() {
     setCard({ value: val, suite: suite });
   };
   const handleChangeValue2 = (event) => {
-    console.log(event);
-    let val = event.target.value;
-    let suite = card.suite;
-    setCard2({ value: val, suite: suite });
+    let val2 = event.target.value;
+    let suite2 = card2.suite;
+    setCard2({ value: val2, suite: suite2 });
   };
   const handleChangeSuite2 = (event) => {
-    let suite = event.target.value;
-    let val = card.value;
-    setCard2({ value: val, suite: suite });
+    let suite2 = event.target.value;
+    let val2 = card2.value;
+    setCard2({ value: val2, suite: suite2 });
   };
-  const changeName = (event) => {
+  /* const changeName = (event) => {
     setName(event.target.value);
-  };
+  }; */
   const handleSubmit = () => {
+    setResults('Loading...');
+    if (players === '') {
+      return 'You need to select an amount of players';
+    }
     let cardOne = card.value + card.suite;
     let cardTwo = card2.value + card2.suite;
-    setCards({});
+    if (cardOne === cardTwo) {
+      setResults('That hand is impossible');
+    } else {
+      let handString = cardOne + cardTwo;
+      console.log(handString);
+      setResults(calculateOdds(players, 10, handString, []));
+      console.log(userReturn);
+    }
+  };
+  const handleSubmitOnce = () => {
+    setResults('Loading...');
+    if (players === '') {
+      return 'You need to select an amount of players';
+    }
+    let cardOne = card.value + card.suite;
+    let cardTwo = card2.value + card2.suite;
+    if (cardOne === cardTwo) {
+      setResults('That hand is impossible');
+    } else {
+      let handString = cardOne + cardTwo;
+      console.log(handString);
+      setResults(calculateOdds(players, 1, handString, []));
+    }
   };
   const cardValues = [
     { value: '2', label: '2' },
@@ -132,13 +227,13 @@ function Odds() {
   //   const player2Odds = result.equities[1].getEquity();
   //   console.log(player2Odds);
   return (
-    <div>
-      <TextField
+    <div className>
+      {/* <TextField
         value={name}
         onChange={changeName}
         label="Player Name"
       ></TextField>
-      <InputLabel>Card Value</InputLabel>
+      <InputLabel>Card Value</InputLabel> */}
       <Select value={card.value} onChange={handleChangeValue}>
         {cardValues.map((card) => {
           return <MenuItem value={card.value}> {card.label} </MenuItem>;
@@ -154,12 +249,19 @@ function Odds() {
           return <MenuItem value={card.value}> {card.label} </MenuItem>;
         })}
       </Select>
-      <Select value={card.suite} onChange={handleChangeSuite2}>
+      <Select value={card2.suite} onChange={handleChangeSuite2}>
         {suites.map((suite) => {
           return <MenuItem value={suite.value}> {suite.label} </MenuItem>;
         })}
       </Select>
-      <Button onClick={handleSubmit}>Add Card</Button>
+      <Select value={players} onChange={handlePlayer}>
+        {[2, 3, 4, 5, 6, 7, 8].map((num) => {
+          return <MenuItem value={num}> {num} </MenuItem>;
+        })}
+      </Select>
+      <Button onClick={handleSubmitOnce}>Simulate Round Odds</Button>
+      <Button onClick={handleSubmit}>Calculate Odds Estimation</Button>
+      <p> {oddsResults}</p>
     </div>
   );
 }
